b3bae19af3cc4eded41ec0490b22a718294a435b
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-24 15:18:05 -0400

    update view

--- bot.js
+++ bot.js
@@ -1,49 +1,30 @@
 const { Telegraf, Markup } = require("telegraf");
-const http = require("http");
+const path = require("path");
 
-/* const botApp = process.env['APP']
-const token = process.env['BOT_TOKEN']; */
 const botApp = "https://t.me/UniSwapbot/UniSwapApp";
 const token = "6918635120:AAHhIx8csASASWBp3LTeRxBLjWv0Jm3jCjg";
 
 const bot = new Telegraf(token);
 
 bot.start((ctx) => {
-  ctx.replyWithMarkdown(
-    `👛 Receive, send, store, play and win with cryptocurrencies at any time. [Learn more ›](https://t.me/UniSwapNew/18)\n\n` +
-      `Join our [channel](http://t.me/UniSwapNew) and our [chat](http://t.me/UniSwaptips). `,
-    Markup.inlineKeyboard([
-      [Markup.button.url("Open Uniswamp", botApp)],
-      [Markup.button.callback("Button 3", "btn_3")],
-      [Markup.button.callback("Button 2", "btn_2")],
-    ])
+  const imagePath = path.join(__dirname, 'photo_2024-08-24_14-40-26.jpg');
+  
+  ctx.replyWithPhoto(
+    { source: imagePath }, 
+    {
+      caption: `👏 Minting a new beginning on [Telegram](https://t.me/durov)\n\n` +
+      ` We announce the official launch of the Mini App version of UniSwap.org.`,
+      parse_mode: 'Markdown', 
+      ...Markup.inlineKeyboard([
+        [Markup.button.url('🦄 Mini App', 'https://t.me/UniSwapbot/UniSwapApp')],
+        [Markup.button.callback('Connect Wallet', 'xxxx'),Markup.button.url('Twitter', 'https://x.com/uniswap')],
+        [Markup.button.url('Chanel', 'https://t.me/UniSwapNew'),Markup.button.url('Suportt', 'https://t.me/UniSwapTips')],
+      ])
+    }
   );
 });
 
-bot.action("btn_2", (ctx) => {
-  ctx.reply("You pressed Button 2");
-});
-
-bot.action("btn_3", (ctx) => {
-  ctx.reply("You pressed Button 3");
-});
 
 bot.launch();
 
 console.log("Bot is running...");
-
-// SERVER
-const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/status") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Bot is running...");
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not Found");
-  }
-});
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
